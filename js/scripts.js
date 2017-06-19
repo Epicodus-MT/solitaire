ab1117b14647f1167d393e78ddf50b98bbd92db6
Mara Timberlake <mara@>
2017-06-19 10:44:32 -0700

    create and test working method to initialize a deck and noting ace cards

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,15 @@ function GameBoard () {
   this.row7 = [];
 }
 
+
+function Card (suit,number,shade){
+  this.suit = suit;
+  this.number = number;
+  this.shade = shade
+  if (this.number === 13){ this.ace = true;}
+  this.faceDown = true;
+}
+
 function CardDeck () {
   this.deck = [0];
 }
@@ -15,18 +24,25 @@ function CardDeck () {
 CardDeck.prototype.initialize = function(){
   for (let x = 1; x < 53;x++){
     if (x <= 13){
-      this.deck[x] = new Card("spades",x,"black");
+      card = new Card("spades",x,"black");
+      this.deck.push(card);
+    } else if (x <= 26 && x > 13) {
+      card = new Card("hearts", x - 13, "red");
+      this.deck.push(card);
+    } else if (x <= 39 && x > 26) {
+      card = new Card("diamond", x - 26, "red");
+      this.deck.push(card);
+    } else {
+      card = new Card("clubs", x - 39, "black");
+      this.deck.push(card);
     }
   }
 }
 
-function Card (suit,number,shade){
-  this.suit = suit;
-  this.number = number;
-  this.shade = shade
-  this.faceDown = true;
-}
 
-solitaireDeck = new CardDeck();
-solitaireDeck.initialize();
-console.log(solitaireDeck.deck);
+
+$(document).ready(function(){
+  solitaire = new CardDeck();
+  solitaire.initialize();
+  console.log(solitaire.deck);
+})
