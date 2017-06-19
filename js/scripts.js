8eac391450d2d0a2e5d8f5238b9bc0fc81e7d4fa
Mara Timberlake <mara@>
2017-06-19 13:18:03 -0700

    create first draft of startGame prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//The game board will keep track of cards and victory
+
 function GameBoard () {
   this.dealDeck    = [];
   this.discardPile = [];
@@ -14,6 +16,7 @@ function GameBoard () {
   this.clubsRow    = [];
 }
 
+//The card objects
 
 function Card (suit,number,shade){
   this.suit = suit;
@@ -23,41 +26,45 @@ function Card (suit,number,shade){
   this.faceDown = true;
 }
 
+//The card deck
+
 function CardDeck () {
-  this.deck = [0];
+  this.deck = [];
 }
 
+//puts 52 card objects into a deck
+
 CardDeck.prototype.initialize = function(){
-  for (let x = 1; x < 53;x++){
+  for (let x = 0; x < 52;x++){
+    let y = x + 1;
     if (x <= 13){
-      card = new Card("spades",x,"black");
+      card = new Card("spades",y,"black");
       this.deck.push(card);
     } else if (x <= 26 && x > 13) {
-      card = new Card("hearts", x - 13, "red");
+      card = new Card("hearts", y - 13, "red");
       this.deck.push(card);
     } else if (x <= 39 && x > 26) {
-      card = new Card("diamond", x - 26, "red");
+      card = new Card("diamond", y - 26, "red");
       this.deck.push(card);
     } else {
-      card = new Card("clubs", x - 39, "black");
+      card = new Card("clubs", y - 39, "black");
       this.deck.push(card);
     }
   }
 }
 
+//puts the game board together
+
 CardDeck.prototype.startGame = function(gameboard){
-  while (true) {
-    x = Math.floor(Math.random() * (this.deck.length - 1 + 1)) + 1;
-    let y = x + 1;
-    let pulledCard = this.deck.slice(x,y);
+  for (let z = 1; z < 29; z++) {
+    let x = Math.floor(Math.random() * (this.deck.length - 1 + 1)) + 1;
+    let pulledCard = this.deck[x];
     this.deck.splice(x, 1);
     gameboard.row1.push(pulledCard);
-    break;
   }
 }
 
-
-
+//console log testing
 $(document).ready(function(){
   game = new GameBoard();
   solitaire = new CardDeck();
